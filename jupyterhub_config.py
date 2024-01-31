c976b6bc4789c9208edd64d92522f1c6de3b9134
joni <jonatan@jacoti.com>
2024-01-31 21:15:21 +0100

    Added get_env to track environments

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -3,10 +3,33 @@ import sys
 from oauthenticator.google import LocalGoogleOAuthenticator
 from oauthenticator.generic import GenericOAuthenticator
 
+
+env_types= [str | int | float | bool | None]
+
+def parse_bool(value: env_types) -> bool:
+    if value.lower() in ('true', 'yes', '1'):
+        return True
+    return False
+
+
+def get_env(
+        name: str, default: env_types = None, var_type: type = str
+) -> env_types:
+    if name not in os.environ and default is None:
+        raise EnvironmentError('Missing environment variable: ' + name)
+    var_type = parse_bool if var_type is bool else var_type
+    try:
+        return var_type(get_env(name, default=default))
+    except ValueError:
+        raise EnvironmentError(
+            'Invalid value for environment variable: ' + name
+        )
+
+
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
-c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_IMAGE']
-c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
-c.JupyterHub.hub_ip = os.environ['HUB_IP']
+c.DockerSpawner.image = get_env('DOCKER_JUPYTER_IMAGE')
+c.DockerSpawner.network_name = get_env('DOCKER_NETWORK_NAME')
+c.JupyterHub.hub_ip = get_env('HUB_IP')
 
 # Redirect to JupyterLab, instead of the plain Jupyter notebook
 c.Spawner.default_url = '/lab'
@@ -46,60 +69,60 @@ c.JupyterHub.services = [
 # ]
 
 if (
-        os.getenv('CLIENT_ID')
-        and os.getenv('CLIENT_SECRET')
+        get_env('CLIENT_ID')
+        and get_env('CLIENT_SECRET')
 ):
     c.JupyterHub.authenticator_class = GenericOAuthenticator
     c.JupyterHub.authenticator_class.login_service = 'Jaguar in the Jupyter'
     c.JupyterHub.authenticator_class.create_system_users = True
-    c.JupyterHub.authenticator_class.hosted_domain = os.getenv('HOSTED_DOMAINS').split(',')
+    c.JupyterHub.authenticator_class.hosted_domain = get_env('HOSTED_DOMAINS').split(',')
 
-    c.JupyterHub.authenticator_class.client_id = os.getenv('CLIENT_ID')
-    c.JupyterHub.authenticator_class.client_secret = os.getenv('CLIENT_SECRET')
+    c.JupyterHub.authenticator_class.client_id = get_env('CLIENT_ID')
+    c.JupyterHub.authenticator_class.client_secret = get_env('CLIENT_SECRET')
 
-    c.JupyterHub.authenticator_class.oauth_callback_url = os.getenv('REDIRECT_URL')
-    c.JupyterHub.authenticator_class.authorize_url = os.getenv('AUTHORIZE_URL')
-    c.JupyterHub.authenticator_class.token_url = os.getenv('TOKEN_URL')
-    c.JupyterHub.authenticator_class.userdata_url = os.getenv('USERDATA_URL')
-    c.JupyterHub.authenticator_class.username_claim = os.getenv('USERNAME_CLAIM')
-    c.JupyterHub.authenticator_class.scope = os.getenv('SCOPE').split(',')
+    c.JupyterHub.authenticator_class.oauth_callback_url = get_env('REDIRECT_URL')
+    c.JupyterHub.authenticator_class.authorize_url = get_env('AUTHORIZE_URL')
+    c.JupyterHub.authenticator_class.token_url = get_env('TOKEN_URL')
+    c.JupyterHub.authenticator_class.userdata_url = get_env('USERDATA_URL')
+    c.JupyterHub.authenticator_class.username_claim = get_env('USERNAME_CLAIM')
+    c.JupyterHub.authenticator_class.scope = get_env('SCOPE').split(',')
 
     c.Authenticator.add_user_cmd = ['adduser', '-q', '--gecos', '""', '--disabled-password', '--force-badname']
-    c.Authenticator.whitelist = set(os.getenv('AUTHORIZED_USERS').split(','))
-    c.Authenticator.admin_users = set(os.getenv('ADMIN_USERS').split(','))
+    c.Authenticator.whitelist = set(get_env('AUTHORIZED_USERS').split(','))
+    c.Authenticator.admin_users = set(get_env('ADMIN_USERS').split(','))
 
 elif (
-        os.getenv('GOOGLE_CLIENT_ID')
-        and os.getenv('GOOGLE_SECRET')
+        get_env('GOOGLE_CLIENT_ID')
+        and get_env('GOOGLE_SECRET')
 ):
     c.JupyterHub.authenticator_class = LocalGoogleOAuthenticator
     c.JupyterHub.authenticator_class.login_service = 'Jaguar in the Jupyter'
     c.JupyterHub.authenticator_class.create_system_users = True
-    c.JupyterHub.authenticator_class.hosted_domain = os.getenv('HOSTED_DOMAINS').split(',')
+    c.JupyterHub.authenticator_class.hosted_domain = get_env('HOSTED_DOMAINS').split(',')
 
     c.LocalGoogleOAuthenticator.create_system_users = True
     c.LocalGoogleOAuthenticator.hosted_domain = ['jupyter.jaguarintheloop.live', 'gmail.com']
     c.LocalGoogleOAuthenticator.login_service = 'Jaguar in the Jupyter'
 
-    c.LocalGoogleOAuthenticator.oauth_callback_url = os.getenv('GOOGLE_OAUTH_CALLBACK')
-    c.LocalGoogleOAuthenticator.client_id = os.getenv('GOOGLE_CLIENT_ID')
-    c.LocalGoogleOAuthenticator.client_secret = os.getenv('GOOGLE_SECRET')
+    c.LocalGoogleOAuthenticator.oauth_callback_url = get_env('GOOGLE_OAUTH_CALLBACK')
+    c.LocalGoogleOAuthenticator.client_id = get_env('GOOGLE_CLIENT_ID')
+    c.LocalGoogleOAuthenticator.client_secret = get_env('GOOGLE_SECRET')
 
     c.Authenticator.add_user_cmd = ['adduser', '-q', '--gecos', '""', '--disabled-password', '--force-badname']
-    c.Authenticator.whitelist = set(os.getenv('AUTHORIZED_USERS').split(','))
-    c.Authenticator.admin_users = set(os.getenv('ADMIN_USERS').split(','))
+    c.Authenticator.whitelist = set(get_env('AUTHORIZED_USERS').split(','))
+    c.Authenticator.admin_users = set(get_env('ADMIN_USERS').split(','))
 
 
 else:
     # Admin users
-    c.Authenticator.admin_users = set(os.getenv('ADMIN_USERS').split(','))
+    c.Authenticator.admin_users = set(get_env('ADMIN_USERS').split(','))
 
 
 notebook_dir = '/home/jovyan'
 c.DockerSpawner.notebook_dir = notebook_dir
 c.DockerSpawner.volumes = {
-    f'{os.getenv("USER_BASE_DIR")}-{{username}}': "/home/jovyan/work",
-    os.getenv('SHARED_DIR'): "/home/jovyan/public",
+    f'{get_env("USER_BASE_DIR")}-{{username}}': "/home/jovyan/work",
+    get_env('SHARED_DIR'): "/home/jovyan/public",
 }
 
 c.DockerSpawner.environment = {
